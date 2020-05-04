9b131533bf994f6925d3e606f114e441aa0ba35b
Ibraheem Olanrewaju <56089175+IbraheemLanre@users.noreply.github.com>
2020-05-04 20:26:20 +0200

    School Account Mgt System
    This is a backend for how a school would manage the payment system for both teachers and students

--- Student.java
+++ Student.java
@@ -1,20 +1,56 @@
-package org.redischool.coursebookingsystem;
+package School_Management_System;
 
-class Student {
-    String name;
-    int age;
-    String country;
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
 
-    Student(int studentId, String name, int age, String country){
+    public Student(int id, String name, int grade) {
+        this.id = id;
         this.name = name;
-        this.age = age;
-        this.country = country;
+        this.grade = grade;
+        feesPaid = 0;
+        feesTotal = 20000;
     }
 
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 
-    public String toString(){
-        return ("Student " + this.name
-                + " is " + this.age
-                + " years old and comes from " + this.country);
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
+        School.updateTotalMoneyEarned(feesPaid);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    public int getFeesTotal() {
+        return feesTotal;
+    }
+
+    public int getOwedFees() {
+        return feesTotal-feesPaid;
+    }
+
+    @Override
+    public String toString() {
+        return "Student's name: " + "\n" +name+ " Total fees paid so far is " + feesPaid +" Naira"
+                + " and owes " + getOwedFees() + " Naira";
     }
 }
